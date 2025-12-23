4b39188ed6bb58df15d188a1186b7bdf4821d261
adamabiyuu <advmabiyu@gmail.com>
2025-12-24 06:55:51 +0700

    Finalizing res api

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,40 @@
 import express from 'express'
-import { routes } from './routes'
-import bodyParser from 'body-parser'
-import cors from 'cors'
-import connectDB from './utils/connectDB'
-import deserializeToken from './middleware/deserializedToken'
 
 const app = express()
 
-connectDB()
-
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cors())
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  res.setHeader('Access-Control-Allow-Methods', '*')
-  res.setHeader('Access-Control-Allow-Headers', '*')
-  next()
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK' })
 })
 
-app.use(deserializeToken)
-
-routes(app)
-
 export default app
+
+// import express from 'express'
+// import { routes } from './routes'
+// import bodyParser from 'body-parser'
+// import cors from 'cors'
+// import connectDB from './utils/connectDB'
+// import deserializeToken from './middleware/deserializedToken'
+
+// const app = express()
+
+// connectDB()
+
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json())
+// app.use(cors())
+
+// app.use((req, res, next) => {
+//   res.setHeader('Access-Control-Allow-Origin', '*')
+//   res.setHeader('Access-Control-Allow-Methods', '*')
+//   res.setHeader('Access-Control-Allow-Headers', '*')
+//   next()
+// })
+
+// app.use(deserializeToken)
+
+// routes(app)
+
+// export default app
 
 // import express from 'express'
 // import { routes } from './routes'
