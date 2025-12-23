bd2b1706de0269bc338e9ae39d6891d755a2ffc3
adamabiyuu <advmabiyu@gmail.com>
2025-12-24 06:52:53 +0700

    Finalizing res api

--- src/utils/connectDB.ts
+++ src/utils/connectDB.ts
@@ -1,14 +1,35 @@
-import mongoose, { mongo } from 'mongoose'
+import mongoose from 'mongoose'
 import config from '../config/environment'
 import { logger } from './logger'
 
-mongoose
-  .connect(`${config.db}`)
-  .then(() => {
-    logger.info('Connected to mongoDB')
-  })
-  .catch((error) => {
-    logger.info('Could not connect to DB')
+const connectDB = async () => {
+  if (!config.db) {
+    logger.error('❌ DB environment variable is missing')
+    return
+  }
+
+  try {
+    await mongoose.connect(config.db)
+    logger.info('Connected to MongoDB')
+  } catch (error) {
+    logger.error('Could not connect to DB')
     logger.error(error)
-    process.exit(1)
-  })
+  }
+}
+
+export default connectDB
+
+// import mongoose, { mongo } from 'mongoose'
+// import config from '../config/environment'
+// import { logger } from './logger'
+
+// mongoose
+//   .connect(`${config.db}`)
+//   .then(() => {
+//     logger.info('Connected to mongoDB')
+//   })
+//   .catch((error) => {
+//     logger.info('Could not connect to DB')
+//     logger.error(error)
+//     // process.exit(1)
+//   })
