9757c4f2b969b6653f874bdb1da3e2877eae3e55
adamabiyuu <advmabiyu@gmail.com>
2025-12-24 07:12:56 +0700

    bismillah

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import deserializeToken from './middleware/deserializedToken'
 import connectDB from './utils/connectDB'
 
 const app = express()
 
 connectDB()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+app.use(deserializeToken)
 
 app.get('/health', (req, res) => {
   res.json({ status: 'OK' })
