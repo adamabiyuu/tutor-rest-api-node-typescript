2d27023033bc9f5878a854fdd15912e88b085632
adamabiyuu <advmabiyu@gmail.com>
2025-12-24 07:00:58 +0700

    bismillah

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,25 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import deserializeToken from './middleware/deserializedToken'
 import connectDB from './utils/connectDB'
 
 const app = express()
 
 connectDB()
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', '*')
+  next()
+})
+
+app.use(deserializeToken)
 
 app.get('/health', (req, res) => {
   res.json({ status: 'OK' })
